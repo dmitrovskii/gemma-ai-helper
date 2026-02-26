0b45945c0d04baa6e797fb7a24b2b3eb66f7d2ff
dmitrovskii <deniskrot99@gmail.com>
2026-02-26 18:42:26 +0200

    feat: add save dialog

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
+import json
 from google import genai
+from collections import deque
 
 ROOT = pathlib.Path(__file__).resolve().parent
 
@@ -18,6 +20,7 @@ class GoogleTerminalGets:
         return self._config
 
     def ask(self, prompt):
+        full_response = ''
         try:
             print("\n[Gemma]: ", flush=True)
             response = self.client.models.generate_content_stream(
@@ -28,20 +31,35 @@ class GoogleTerminalGets:
             for chunk in response:
                 if chunk.text:
                     print(chunk.text, end="", flush=True)
+                    full_response += chunk.text
             print("\n")
+            return full_response
+
         except Exception as e:
             print(f"\r\033[31m[Помилка API]: {e}\033[0m\n")
+
+    def save_response(self, info):
+        with open(ROOT / 'context.json', 'w', encoding='utf-8') as f:
+            json.dump(list(info), f, indent=4, ensure_ascii=False)
 
 def main():
     GEMINI_API_KEY = (ROOT / 'key.txt').read_text(encoding='utf-8').strip()
     grequest = GoogleTerminalGets(GEMINI_API_KEY)
+    window = deque(maxlen=20)
     try:
         while True:
-            user = input("[You]: ")
-            grequest.ask(user)
+            prompt = input("[You]: ")
+            response = grequest.ask(prompt)
+            data_to_save = [
+                {"role": "user", "parts": prompt},
+                {"role": "model", "parts": response}
+            ]
+            window.extend(data_to_save)
 
     except KeyboardInterrupt:
         print("\nClosing program...")
+    finally: 
+        grequest.save_response(window)
 
 if __name__ == '__main__':
     main()
