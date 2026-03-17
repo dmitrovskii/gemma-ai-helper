cb475eadc18bbac55bc3e15ce50001443bb8c18e
dmitrovskii <deniskrot99@gmail.com>
2026-03-17 16:57:50 +0200

    feat: add functional cli chat

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-from genai import GoogleTerminalGets
+from engine import GoogleTerminalGets
+from engine import ChatMemory
+from google.genai import types
 
 def main():
-    grequest = GoogleTerminalGets()
+
+    model = GoogleTerminalGets()
+    chat = ChatMemory() 
+
     try:
+        instructions = [
+            types.Content(role="user", parts=[types.Part(text=f"SYSTEM INSTRUCTION: {model.instruct}")])
+        ]
+        
+        history = chat.history
+        
         while True:
-            prompt = input("[You]: ")
-            response = grequest.ask(prompt)
-            print(f"\n[Gemma]: {response}")
+            user_text = input("[User]: ")
+            history.extend([
+                types.Content(role="user", parts=[types.Part(text=user_text)])
+            ])
+
+            response = model.ask(
+                [*instructions, *history]
+            )  
+
+            history.extend([
+                types.Content(role="user", parts=[types.Part(text=response)])
+            ])
+
+            print(f"\n[Model]: {response}")
 
     except KeyboardInterrupt:
-        print("\nClosing program...")
+        print("\nClose...")
+    finally:
+        chat.save_history(chat.convert(history))
 
 if __name__ == '__main__':
     main()
